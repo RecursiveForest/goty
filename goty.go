201cd43fac1655c9455a0df1f904edb4b3c381ab
Peter Bourgon <peter@bourgon.org>
2012-02-03 16:20:11 +0100

    gofix for weekly + minor other changes

--- goty.go
+++ goty.go
@@ -1,19 +1,19 @@
 package goty
 
 import (
-	"net"
-	"fmt"
-	"os"
 	"bufio"
+	"fmt"
+	"net"
+	"os"
 	"strings"
 )
 
 type IRCConn struct {
-	Sock *net.TCPConn
+	Sock        *net.TCPConn
 	Read, Write chan string
 }
 
-func Dial(server, nick string) (*IRCConn, os.Error) {
+func Dial(server, nick string) (*IRCConn, error) {
 	read := make(chan string, 1000)
 	write := make(chan string, 1000)
 	con := &IRCConn{nil, read, write}
@@ -21,8 +21,8 @@ func Dial(server, nick string) (*IRCConn, os.Error) {
 	return con, err
 }
 
-func (con *IRCConn) Connect(server, nick string) os.Error {
-	if raddr, err := net.ResolveTCPAddr(server); err != nil {
+func (con *IRCConn) Connect(server, nick string) error {
+	if raddr, err := net.ResolveTCPAddr("tcp", server); err != nil {
 		return err
 	} else {
 		if c, err := net.DialTCP("tcp", nil, raddr); err != nil {
@@ -34,18 +34,14 @@ func (con *IRCConn) Connect(server, nick string) os.Error {
 
 			go func() {
 				for {
-					if closed(con.Read) {
-						fmt.Fprintf(os.Stderr, "goty: read closed\n")
-						break
-					}
 					if str, err := r.ReadString(byte('\n')); err != nil {
-						fmt.Fprintf(os.Stderr, "goty: read: %s\n", err.String())
+						fmt.Fprintf(os.Stderr, "goty: read: %s\n", err.Error())
 						break
 					} else {
 						if strings.HasPrefix(str, "PING") {
 							con.Write <- "PONG" + str[4:len(str)-2]
 						} else {
-							con.Read <- str[0:len(str)-2]
+							con.Read <- str[0 : len(str)-2]
 						}
 					}
 				}
@@ -53,13 +49,13 @@ func (con *IRCConn) Connect(server, nick string) os.Error {
 
 			go func() {
 				for {
-					str := <-con.Write
-					if closed(con.Write) {
+					str, ok := <-con.Write
+					if !ok {
 						fmt.Fprintf(os.Stderr, "goty: write closed\n")
 						break
 					}
 					if _, err := w.WriteString(str + "\r\n"); err != nil {
-						fmt.Fprintf(os.Stderr, "goty: write: %s\n", err.String())
+						fmt.Fprintf(os.Stderr, "goty: write: %s\n", err.Error())
 						break
 					}
 					w.Flush()
@@ -73,7 +69,7 @@ func (con *IRCConn) Connect(server, nick string) os.Error {
 	return nil
 }
 
-func (con *IRCConn) Close() os.Error {
+func (con *IRCConn) Close() error {
 	close(con.Read)
 	close(con.Write)
 	return con.Sock.Close()
